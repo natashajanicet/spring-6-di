29e79bffe73c814465ca07bd0f0c1bd16099dd00
Natasha <natashajanicetambunan@gmail.com>
2023-03-01 21:43:20 +0700

    Create service that is manually injected and create test for it

---
+++ src/main/java/natashalearn/springframework/spring6di/services/GreetingService.java
@@ -0,0 +1,5 @@
+package natashalearn.springframework.spring6di.services;
+
+public interface GreetingService {
+    String sayGreeting();
+}
---
+++ src/test/java/natashalearn/springframework/spring6di/controllers/MyControllerTest.java
@@ -0,0 +1,15 @@
+package natashalearn.springframework.spring6di.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyControllerTest {
+
+    @Test
+    void sayHello() {
+        MyController myController = new MyController();
+
+        myController.sayHello();
+    }
+}
